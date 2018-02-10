79e4422003aaddc50f6953e4d3f04acb363041b6
Pauletta Riggs <riggs20@gmail.com>
2018-02-09 23:12:00 -0700

    Adding new and updated files

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,15 @@
+/*
+Your htmlRoutes.js file should include two routes:
+
+A GET Route to /survey which should display the survey page.
+
+A default, catch-all route that leads to home.html which displays the home page.
+*/
+
+app.get("/survey", function(req, res) {
+    res.sendFile(path.join(__dirname, "survey.html"))
+})
+
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "home.html"));
+});
